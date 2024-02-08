41fb11ab1be86c0418aab59067ca40919ef24db9
Rafael98-stack <rafaelcasapaojr@gmail.com>
2024-02-08 16:27:27 +0100

    Developing

--- src/main/java/it/be/epicode/EsercizioQuattro/DAO/RistoranteService.java
+++ src/main/java/it/be/epicode/EsercizioQuattro/DAO/RistoranteService.java
@@ -8,40 +8,23 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service // Specializzazione di @Component
-// Il service ci serve per poter aggiungere eventuale logica addizionale custom, prima dei metodi del DAO
+@Service
 @Slf4j
 public class RistoranteService {
     @Autowired
     private RistoranteDAO ristoranteDAO;
 
-    public void saveUser(User newUser) {
-        // Nel caso della save, eventuale logica addizionale custom potrebbe essere ad esempio aggiunta di campi extra allo user
-        // oppure controlli vari
-        if (this.existsByEmail(newUser.getEmail())) {
-            throw new RuntimeException("L'email " + newUser.getEmail() + " è già in uso!");
-        } else {
+    public void saveUser(Common newUser) {
+
             if (newUser.getName().length() < 2)
                 throw new RuntimeException("Il nome non può essere più corto di 2 caratteri!");
-            newUser.setAvatarURL("https://picsum.photos/200/300");
-            usersDAO.save(newUser);
+
+            ristoranteDAO.save(newUser);
             log.info("Utente salvato correttamente!");
-        }
+
 
     }
 
-    public Common findById(long id) throws ItemNotFoundException {
-	/*	Optional<User> found = usersDAO.findById(userId);
-		if (found.isPresent()) {
-			// ritorno utente
-			return found.get(); // Se ho la certezza che lo user non sia null, con la get converto l'optional in user
-		} else {
-			// utente non trovato
-			throw new ItemNotFoundException(userId);
-		}*/
-
-        return RistoranteDAO.findById(id).orElseThrow(() -> new ItemNotFoundException(Id)); // Alternativa molto più compatta al codice di sopra
-    }
 
 
 
